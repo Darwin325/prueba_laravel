c3b16af365f5bdb0a7f36d1eb369c47852368531
Darwin Zamora <darwinzc325@gmail.com>
2022-03-03 22:39:50 -0500

    Factoria y seeder para el modelo Porduto

---
+++ database/factories/ProductoFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Producto>
+ */
+class ProductoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $productos = [
+            'teclado', 'mouse', 'audifono', 'monitor', 'cable HDMI', 'cable de poder',
+            'luz led', 'teclado con luces', 'calculadora', 'celular', 'cargador celular',
+            'cargador computador', 'forro celular', 'forro computador'
+        ];
+        return [
+            'nombre' => collect($productos)->random(),
+            'descipcion' => $this->faker->paragraph,
+            'foto' => $this->faker->url,
+            'precio' => $this->faker->randomNumber(5),
+            'iva' => collect([15,19,21])->random(),
+            'vendedor_id' => User::all()->random()->id
+        ];
+    }
+}
---
+++ database/seeders/ProductoSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Producto;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+
+class ProductoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Producto::factory(10)->create([
+           'foto' => asset(Storage::url('foto.jpg'))
+        ]);
+    }
+}
